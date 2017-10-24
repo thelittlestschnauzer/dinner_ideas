11aa137e2fe6e5a1f32b1b325f600a5ea016b914
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-24 13:56:15 -0400

    stubbed the data for the different cuisines

--- lib/dinner_ideas/cli.rb
+++ lib/dinner_ideas/cli.rb
@@ -8,7 +8,19 @@ class DinnerIdeas::CLI
     puts "Discover the world through food.".blue.bold
     puts "\nChoose a cuisine and find a recipe for dinner tonight, 1-10.".blue.bold
     #scrape the cuisines listed on Allrecipes here
-    
+    #http://allrecipes.com/recipes/86/world-cuisine/?internalSource=hub%20nav&referringId=227&referringContentType=recipe%20hub&referringPosition=3&linkName=hub%20nav%20exposed&clickId=hub%20nav%203
+    puts <<~DOC
+    1. Mexican
+    2. Italian
+    3. Chinese
+    4. Indian
+    5. Thai
+    6. Asian
+    7. Latin American
+    8. Middle Eastern
+    9. African
+    10. European
+    DOC
 
   end
 
