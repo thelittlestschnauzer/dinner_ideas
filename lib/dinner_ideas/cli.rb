285d955342a6832fd5d43b643be0a16d9fc585ae
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-10-24 14:12:43 -0400

    stubbing out the user input method menu

--- lib/dinner_ideas/cli.rb
+++ lib/dinner_ideas/cli.rb
@@ -2,11 +2,11 @@ class DinnerIdeas::CLI
 
   def call
     list_world_cuisine
+    menu
   end
 
   def list_world_cuisine
-    puts "Discover the world through food.".blue.bold
-    puts "\nChoose a cuisine and find a recipe for dinner tonight, 1-10.".blue.bold
+    puts "What is for dinner tonight?.".blue.bold
     #scrape the cuisines listed on Allrecipes here
     #http://allrecipes.com/recipes/86/world-cuisine/?internalSource=hub%20nav&referringId=227&referringContentType=recipe%20hub&referringPosition=3&linkName=hub%20nav%20exposed&clickId=hub%20nav%203
     puts <<~DOC
@@ -21,8 +21,12 @@ class DinnerIdeas::CLI
     9. African
     10. European
     DOC
-
   end
 
+  def menu
+    puts "Enter the number of the type of cuisine you want recipes from: ".blue.bold
+    input = gets.strip.downcase
+    
+  end
 
 end
